88432c60676d97bae878d0f2a2ee8ed7fb1aec42
adder <temartem68@mail.ru>
2022-05-19 16:09:04 +0300

    roulette and craps

--- craps.py
+++ craps.py
@@ -1,5 +1,5 @@
 import random
-
+from art import *
 from matplotlib import pyplot as plt
 
 
@@ -17,20 +17,23 @@ def move():
             return False
 
 
-money_total = 100
-win = 0
-loss = 0
-for p in range(100):
-    bet = 1
-    step = move()
-    if step is True:
-        money_total += bet
-        win += 1
-    else:
-        money_total -= bet
-        loss += 1
-print("Win", win, "Loss", loss, "Money", money_total)
+if __name__ == '__main__':
+    Art = text2art("Craps")
+    print(Art)
+    money_total = 100
+    win = 0
+    loss = 0
+    for p in range(100):
+        bet = 1
+        step = move()
+        if step is True:
+            money_total += bet
+            win += 1
+        else:
+            money_total -= bet
+            loss += 1
+    print("Win", win, "Loss", loss, "Money", money_total)
 
-y = [shoot() for v in range(100)]
-plt.hist(y)
-plt.show()
+    y = [shoot() for v in range(100)]
+    plt.hist(y)
+    plt.show()
